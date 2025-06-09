44de060f3f750b86a02348de4452cfd15f2052b4
shaeantoine <shaeantoine@gmail.com>
2025-06-09 17:09:50 +1000

    Update data download method

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import pickle
 import tarfile
 import numpy as np
@@ -25,18 +26,22 @@ def download_cifar100():
 
 # Load and preprocess dataset
 def load_cifar100(split="train"):
+    print("Splitting the dataset...")
     assert split in ["train", "test"]
     with open(EXTRACTED_DIR / f"{split}", "rb") as f:
         data_dict = pickle.load(f, encoding="latin1")
     
     # Data: shape (N, 3, 32, 32), Labels: shape (N,)
+    print("Splitting the dataset... Batch 1")
     data = data_dict["data"].reshape(-1, 3, 32, 32).astype(np.float32)
     labels = np.array(data_dict["fine_labels"], dtype=np.int32)
 
     # Resizing images
+    print("Splitting the dataset... Batch 2")
     data = resize_image(data)
 
     # Normalize to [0, 1] then apply ImageNet normalization
+    print("Splitting the dataset... Batch 3")
     data /= 255.0
     mean = np.array([0.485, 0.456, 0.406])[:, None, None]
     std = np.array([0.229, 0.224, 0.225])[:, None, None]
@@ -59,13 +64,19 @@ def make_batches(data, labels, batch_size, shuffle=True):
 
 
 def resize_image(data, target_size=(224,224)):
+    print("Resizing the dataset...")
     resized = []
-    for img in data:
+    for idx, img in enumerate(data):
         # Convert (3, 32, 32) to (32, 32, 3)
         img = np.transpose(img, (1, 2, 0))
         pil_img = Image.fromarray((img * 255).astype(np.uint8))
         pil_img = pil_img.resize(target_size, Image.BICUBIC)
         img_resized = np.array(pil_img).astype(np.float32) / 255.0
-        img_resized = np.transpose(img_resized, (2, 0, 1))  # back to (3, H, W)
+        img_resized = np.transpose(img_resized, (2, 0, 1))  # (3, 224, 224)
         resized.append(img_resized)
+
+        if idx % 100 == 0:
+            print(f"Changing image size, image number {idx}")
+
+    gc.collect
     return np.stack(resized)
