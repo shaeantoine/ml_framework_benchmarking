368b1835fb6646d67417af0445910bad83fb1bb2
shaeantoine <shaeantoine@gmail.com>
2025-06-10 23:20:58 +1000

    Refactor resize() method

--- data/download_store.py
+++ data/download_store.py
@@ -52,34 +52,29 @@ def batching(images, labels, batch_size=128, shuffle=True):
        
 
 # Resizing dataset
-def resize(images, labels, split, size=(224,224)):
+def resize(batches, split, size=(224, 224)): # Ingest a list of batches (image_batch, label_batch)
     os.makedirs(CACHE_DIR, exist_ok=True)
-    cache_img_path = CACHE_DIR / f"{split}_images_{size[0]}x{size[1]}.npy"
-    cache_lbl_path = CACHE_DIR / f"{split}_labels.npy"
 
-    if cache_img_path.exists() and cache_lbl_path.exists():
-        print(f"🔄 Using cached {split} data...")
-        return np.load(cache_img_path), np.load(cache_lbl_path)
+    for i, (images_batch, labels_batch) in enumerate(batches):
+        # Create batch specific directories
+        cache_batch_file_path = os.path.join(CACHE_DIR, f"{split}_batch_{i}_{size[0]}x{size[1]}.npy")
+        
+        resized_images_list = []
+        for img in images_batch: 
+            img = np.transpose(img, (1, 2, 0))  # (C, H, W) to (H, W, C)
+            pil_img = Image.fromarray((img * 255).astype(np.uint8))
+            pil_img = pil_img.resize(size, Image.BICUBIC)
+            resized_img = np.array(pil_img).astype(np.float32) / 255.0
+            resized_img = np.transpose(resized_img, (2, 0, 1))  # (H, W, C) to (C, H, W)
+            resized_images_list.append(resized_img)
 
-    print(f"Resizing {split} images to {size}...")
-    resized = []
-    for i, img in enumerate(images):
-        img = np.transpose(img, (1, 2, 0))  # (32, 32, 3)
-        pil_img = Image.fromarray((img * 255).astype(np.uint8))
-        pil_img = pil_img.resize(size, Image.BICUBIC)
-        resized_img = np.array(pil_img).astype(np.float32) / 255.0
-        resized_img = np.transpose(resized_img, (2, 0, 1))  # (3, H, W)
-        resized.append(resized_img)
+        # Placing resized images back into batch array 
+        resized_images_array = np.stack(resized_images_list)
+        batch_data_to_save = (resized_images_array, labels_batch)
+        np.save(cache_batch_file_path, batch_data_to_save)
 
-        if (i + 1) % 5000 == 0:
-            print(f"{i + 1}/{len(images)} done")
-
-    resized = np.stack(resized)
-    np.save(cache_img_path, resized)
-    np.save(cache_lbl_path, labels)
-    print(f"✅ Saved {split} set to {cache_img_path}")
-    return resized, labels
-
+        print(f"Saved {split} set to {cache_batch_file_path}")
+        
 
 if __name__ == "__main__":
     print("Downloading...")
@@ -88,5 +83,10 @@ if __name__ == "__main__":
     print("Loading raw data...")
     images, labels = load_raw("train")
 
-    print("Resizing raw data...")
-    #resize(images, labels, "train")
+    print("Batching raw data...")
+    batches = list(batching(images, labels)) # List of tuples (images, labels)
+
+    print("Resizing raw batches...")
+    resize(batches, split="train")
+
+
